65fac1951eb4bd8624d23a35b158833ec38cb8ce
Eugene Obrezkov <ghaiklor@gmail.com>
2015-06-25 11:30:22 +0300

    Add test for BasePayment

---
+++ test/BasePayment.test.js
@@ -0,0 +1,56 @@
+var assert = require('chai').assert;
+var BasePayment = require('../lib/BasePayment');
+
+describe('BasePayment', function () {
+  it('Should properly export', function () {
+    assert.isFunction(BasePayment);
+    assert.isFunction(BasePayment.prototype.getConfig);
+    assert.isFunction(BasePayment.prototype.setConfig);
+    assert.isFunction(BasePayment.prototype.checkout);
+    assert.isFunction(BasePayment.prototype.refund);
+
+    assert.throw(function () {
+      BasePayment.prototype.checkout();
+    }, Error);
+
+    assert.throw(function () {
+      BasePayment.prototype.refund();
+    }, Error);
+  });
+
+  it('Should properly make objects configurable', function () {
+    var payment = new BasePayment();
+
+    assert.notOk(payment.getConfig('foo'));
+    assert.instanceOf(payment.setConfig('foo', 'bar'), BasePayment);
+    assert.instanceOf(payment.setConfig('obj', {foo: 'bar'}), BasePayment);
+    assert.deepEqual(payment.getConfig('obj'), {foo: 'bar'});
+    assert.equal(payment.getConfig('obj').foo, 'bar');
+    assert.equal(payment.getConfig('foo'), 'bar');
+  });
+
+  it('Should properly create payment with pre-defined config', function () {
+    var payment = new BasePayment({
+      apiKey: 'API_KEY'
+    });
+
+    assert.equal(payment.getConfig('apiKey'), 'API_KEY');
+    assert.notOk(payment.getConfig('NOT_EXISTS'));
+  });
+
+  it('Should properly throw error on checkout', function () {
+    var payment = new BasePayment();
+
+    assert.throw(function () {
+      payment.checkout();
+    }, Error);
+  });
+
+  it('Should properly throw error on refund', function () {
+    var payment = new BasePayment();
+
+    assert.throw(function () {
+      payment.refund();
+    }, Error);
+  });
+});
