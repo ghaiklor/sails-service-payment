c1f533825349c9531513c4c4aef53e69d6a0a7e4
Eugene Obrezkov <ghaiklor@gmail.com>
2015-06-28 10:49:21 +0300

    Add get/set provider to base payment

--- test/BasePayment.test.js
+++ test/BasePayment.test.js
@@ -7,14 +7,9 @@ describe('BasePayment', function () {
     assert.isFunction(BasePayment.prototype.getConfig);
     assert.isFunction(BasePayment.prototype.setConfig);
     assert.isFunction(BasePayment.prototype.checkout);
-    assert.isFunction(BasePayment.prototype.refund);
 
     assert.throw(function () {
       BasePayment.prototype.checkout();
-    }, Error);
-
-    assert.throw(function () {
-      BasePayment.prototype.refund();
     }, Error);
   });
 
@@ -38,19 +33,19 @@ describe('BasePayment', function () {
     assert.notOk(payment.getConfig('NOT_EXISTS'));
   });
 
+  it('Should properly get/set payment provider', function () {
+    var payment = new BasePayment();
+
+    assert.notOk(payment.getProvider());
+    assert.instanceOf(payment.setProvider('SOME_PROVIDER'), BasePayment);
+    assert.equal(payment.getProvider(), 'SOME_PROVIDER');
+  });
+
   it('Should properly throw error on checkout', function () {
     var payment = new BasePayment();
 
     assert.throw(function () {
       payment.checkout();
-    }, Error);
-  });
-
-  it('Should properly throw error on refund', function () {
-    var payment = new BasePayment();
-
-    assert.throw(function () {
-      payment.refund();
     }, Error);
   });
 });
