2ea8e29c5f65cc18c09a214cb8502bdd31c3f4d1
Eugene Obrezkov <ghaiklor@gmail.com>
2015-07-01 17:54:13 +0300

    Cover braintree with tests

--- test/BrainTreePayment.test.js
+++ test/BrainTreePayment.test.js
@@ -1,4 +1,5 @@
 var assert = require('chai').assert;
+var sinon = require('sinon');
 var BrainTreePayment = require('../lib/BrainTreePayment');
 
 var PROVIDER_CONFIG = {
@@ -17,6 +18,17 @@ var CHECKOUT_CONFIG = {
   cvv: '123'
 };
 
+var BRAINTREE_CHECKOUT_CONFIG = {
+  amount: '10.00',
+  creditCard: {
+    number: '4242424242424242',
+    cardholderName: 'Eugene Obrezkov',
+    expirationMonth: '01',
+    expirationYear: '2018',
+    cvv: '123'
+  }
+};
+
 describe('BrainTreePayment', function () {
   it('Should properly export', function () {
     assert.isFunction(BrainTreePayment);
@@ -24,6 +36,22 @@ describe('BrainTreePayment', function () {
 
   it('Should properly call checkout method', function (done) {
     var payment = new BrainTreePayment(PROVIDER_CONFIG);
-    payment.checkout(CHECKOUT_CONFIG).then(done).catch(done);
+
+    sinon.stub(payment._provider.transaction, 'sale', function (config, cb) {
+      cb();
+    });
+
+    payment
+      .checkout(CHECKOUT_CONFIG)
+      .then(function () {
+        assert(payment._provider.transaction.sale.calledOnce);
+        assert.deepEqual(payment._provider.transaction.sale.getCall(0).args[0], BRAINTREE_CHECKOUT_CONFIG);
+        assert.isFunction(payment._provider.transaction.sale.getCall(0).args[1]);
+
+        payment._provider.transaction.sale.restore();
+
+        done();
+      })
+      .catch(done);
   });
 });
