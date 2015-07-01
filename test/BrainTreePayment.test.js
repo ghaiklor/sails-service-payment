c61ae242af47ad8eae5b6a31bb99a31df1c2b2af
Eugene Obrezkov <ghaiklor@gmail.com>
2015-07-01 15:57:14 +0300

    Implement basic braintree payments

--- test/BrainTreePayment.test.js
+++ test/BrainTreePayment.test.js
@@ -1,8 +1,29 @@
 var assert = require('chai').assert;
 var BrainTreePayment = require('../lib/BrainTreePayment');
 
+var PROVIDER_CONFIG = {
+  sandbox: true,
+  merchantId: '',
+  publicKey: '',
+  privateKey: ''
+};
+
+var CHECKOUT_CONFIG = {
+  amount: '10.00',
+  cardNumber: '4242424242424242',
+  cardHolderName: 'Eugene Obrezkov',
+  expMonth: '01',
+  expYear: '2018',
+  cvv: '123'
+};
+
 describe('BrainTreePayment', function () {
   it('Should properly export', function () {
     assert.isFunction(BrainTreePayment);
+  });
+
+  it('Should properly call checkout method', function (done) {
+    var payment = new BrainTreePayment(PROVIDER_CONFIG);
+    payment.checkout(CHECKOUT_CONFIG).then(done).catch(done);
   });
 });
