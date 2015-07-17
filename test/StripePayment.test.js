19580347f53e4a4b27c7fb716d67975bf2a3bd6d
Eugene Obrezkov <ghaiklor@gmail.com>
2015-07-17 19:06:58 +0300

    Improve test coverage for stripe

--- test/StripePayment.test.js
+++ test/StripePayment.test.js
@@ -6,21 +6,18 @@ var PROVIDER_CONFIG = {
   apiKey: ''
 };
 
-var CHECKOUT_CONFIG = {
-  description: 'TEST',
-  amount: 100 * 10,
+var CREDIT_CARD = {
+  amount: 100 * 15.35,
   cardNumber: '4242424242424242',
   cardHolderName: 'Eugene Obrezkov',
   expMonth: '01',
   expYear: '2018',
-  cvv: '123',
-  currency: 'usd'
+  cvv: '123'
 };
 
 var CHECKOUT_CONFIG_SHOULD_BE = {
-  amount: 100 * 10,
+  amount: 1535,
   currency: 'usd',
-  description: 'TEST',
   capture: true,
   source: {
     object: 'card',
@@ -33,9 +30,8 @@ var CHECKOUT_CONFIG_SHOULD_BE = {
 };
 
 var CHECKOUT_CONFIG_EXTENDED_SHOULD_BE = {
-  amount: 100 * 10,
+  amount: 1535,
   currency: 'usd',
-  description: 'TEST',
   capture: true,
   receipt_email: 'ghaiklor@gmail.com',
   source: {
@@ -54,15 +50,16 @@ describe('StripePayment', function () {
   });
 
   it('Should properly make checkout', function (done) {
-    var payment = new StripePayment({provider: PROVIDER_CONFIG});
+    var payment = new StripePayment(PROVIDER_CONFIG);
 
     sinon.stub(payment.getProvider().charges, 'create', function (config, cb) {
-      cb();
+      cb(null, 'CHARGE');
     });
 
     payment
-      .checkout(CHECKOUT_CONFIG)
-      .then(function () {
+      .checkout(CREDIT_CARD)
+      .then(function (charge) {
+        assert.equal(charge, 'CHARGE');
         assert(payment.getProvider().charges.create.calledOnce);
         assert.deepEqual(payment.getProvider().charges.create.getCall(0).args[0], CHECKOUT_CONFIG_SHOULD_BE);
         assert.isFunction(payment.getProvider().charges.create.getCall(0).args[1]);
@@ -74,15 +71,37 @@ describe('StripePayment', function () {
       .catch(done);
   });
 
+  it('Should properly throw exception on checkout', function (done) {
+    var payment = new StripePayment(PROVIDER_CONFIG);
+
+    sinon.stub(payment.getProvider().charges, 'create', function (config, cb) {
+      cb(new Error('Some error occurred'));
+    });
+
+    payment
+      .checkout(CREDIT_CARD)
+      .then(done)
+      .catch(function (error) {
+        assert.instanceOf(error, Error);
+        assert(payment.getProvider().charges.create.calledOnce);
+        assert.deepEqual(payment.getProvider().charges.create.getCall(0).args[0], CHECKOUT_CONFIG_SHOULD_BE);
+        assert.isFunction(payment.getProvider().charges.create.getCall(0).args[1]);
+
+        payment.getProvider().charges.create.restore();
+
+        done();
+      });
+  });
+
   it('Should properly make checkout with extended properties', function (done) {
-    var payment = new StripePayment({provider: PROVIDER_CONFIG});
+    var payment = new StripePayment(PROVIDER_CONFIG);
 
     sinon.stub(payment.getProvider().charges, 'create', function (config, cb) {
       cb();
     });
 
     payment
-      .checkout(CHECKOUT_CONFIG, {
+      .checkout(CREDIT_CARD, {
         receipt_email: 'ghaiklor@gmail.com'
       })
       .then(function () {
@@ -97,19 +116,64 @@ describe('StripePayment', function () {
       .catch(done);
   });
 
+  it('Should properly retrieve info about transaction', function (done) {
+    var payment = new StripePayment(PROVIDER_CONFIG);
+
+    sinon.stub(payment.getProvider().charges, 'retrieve', function (transactionId, cb) {
+      cb(null, 'TRANSACTION');
+    });
+
+    payment
+      .retrieve('TRANSACTION_ID')
+      .then(function (transaction) {
+        assert.equal(transaction, 'TRANSACTION');
+        assert(payment.getProvider().charges.retrieve.calledOnce);
+        assert.deepEqual(payment.getProvider().charges.retrieve.getCall(0).args[0], 'TRANSACTION_ID');
+        assert.isFunction(payment.getProvider().charges.retrieve.getCall(0).args[1]);
+
+        payment.getProvider().charges.retrieve.restore();
+
+        done();
+      })
+      .catch(done);
+  });
+
+  it('Should properly throw exception on getting info about transaction', function (done) {
+    var payment = new StripePayment(PROVIDER_CONFIG);
+
+    sinon.stub(payment.getProvider().charges, 'retrieve', function (transactionId, cb) {
+      cb(new Error('Some error occurred'));
+    });
+
+    payment
+      .retrieve('TRANSACTION_ID')
+      .then(done)
+      .catch(function (error) {
+        assert.instanceOf(error, Error);
+        assert(payment.getProvider().charges.retrieve.calledOnce);
+        assert.deepEqual(payment.getProvider().charges.retrieve.getCall(0).args[0], 'TRANSACTION_ID');
+        assert.isFunction(payment.getProvider().charges.retrieve.getCall(0).args[1]);
+
+        payment.getProvider().charges.retrieve.restore();
+
+        done();
+      });
+  });
+
   it('Should properly call refund method', function (done) {
-    var payment = new StripePayment({provider: PROVIDER_CONFIG});
+    var payment = new StripePayment(PROVIDER_CONFIG);
 
     sinon.stub(payment.getProvider().charges, 'createRefund', function (transactionId, config, cb) {
-      cb();
+      cb(null, 'REFUND');
     });
 
     payment
       .refund('TRANSACTION_ID')
-      .then(function () {
+      .then(function (refund) {
+        assert.equal(refund, 'REFUND');
         assert(payment.getProvider().charges.createRefund.calledOnce);
         assert.equal(payment.getProvider().charges.createRefund.getCall(0).args[0], 'TRANSACTION_ID');
-        assert.deepEqual(payment.getProvider().charges.createRefund.getCall(0).args[1], {});
+        assert.isObject(payment.getProvider().charges.createRefund.getCall(0).args[1]);
         assert.isFunction(payment.getProvider().charges.createRefund.getCall(0).args[2]);
 
         payment.getProvider().charges.createRefund.restore();
@@ -117,5 +181,28 @@ describe('StripePayment', function () {
         done();
       })
       .catch(done);
+  });
+
+  it('Should properly throw exception on refund', function (done) {
+    var payment = new StripePayment(PROVIDER_CONFIG);
+
+    sinon.stub(payment.getProvider().charges, 'createRefund', function (transactionId, config, cb) {
+      cb(new Error('Some error occurred'));
+    });
+
+    payment
+      .refund('TRANSACTION_ID')
+      .then(done)
+      .catch(function (error) {
+        assert.instanceOf(error, Error);
+        assert(payment.getProvider().charges.createRefund.calledOnce);
+        assert.equal(payment.getProvider().charges.createRefund.getCall(0).args[0], 'TRANSACTION_ID');
+        assert.isObject(payment.getProvider().charges.createRefund.getCall(0).args[1]);
+        assert.isFunction(payment.getProvider().charges.createRefund.getCall(0).args[2]);
+
+        payment.getProvider().charges.createRefund.restore();
+
+        done();
+      });
   });
 });
