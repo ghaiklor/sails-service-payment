5a5a41e72e0ac314eaea0aa0538a493e5be553c6
Eugene Obrezkov <ghaiklor@gmail.com>
2015-07-17 10:48:30 +0300

    Rewrite BasePayment test

--- test/BasePayment.test.js
+++ test/BasePayment.test.js
@@ -4,63 +4,44 @@ var BasePayment = require('../lib/BasePayment');
 describe('BasePayment', function () {
   it('Should properly export', function () {
     assert.isFunction(BasePayment);
-    assert.isFunction(BasePayment.prototype.getConfig);
-    assert.isFunction(BasePayment.prototype.setConfig);
+    assert.isFunction(BasePayment.prototype.get);
+    assert.isFunction(BasePayment.prototype.set);
     assert.isFunction(BasePayment.prototype.getProvider);
     assert.isFunction(BasePayment.prototype.setProvider);
     assert.isFunction(BasePayment.prototype.checkout);
     assert.isFunction(BasePayment.prototype.refund);
-
-    assert.throw(function () {
-      BasePayment.prototype.checkout();
-    }, Error);
-
-    assert.throw(function () {
-      BasePayment.prototype.refund();
-    }, Error);
   });
 
   it('Should properly make objects configurable', function () {
     var payment = new BasePayment();
 
-    assert.notOk(payment.getConfig('foo'));
-    assert.instanceOf(payment.setConfig('foo', 'bar'), BasePayment);
-    assert.instanceOf(payment.setConfig('obj', {foo: 'bar'}), BasePayment);
-    assert.deepEqual(payment.getConfig('obj'), {foo: 'bar'});
-    assert.equal(payment.getConfig('obj').foo, 'bar');
-    assert.equal(payment.getConfig('foo'), 'bar');
+    assert.notOk(payment.get('foo'));
+    assert.instanceOf(payment.set('foo', 'bar'), BasePayment);
+    assert.instanceOf(payment.set('obj', {foo: 'bar'}), BasePayment);
+    assert.deepEqual(payment.get('obj'), {foo: 'bar'});
+    assert.equal(payment.get('obj.foo'), 'bar');
+    assert.equal(payment.get('foo'), 'bar');
   });
 
   it('Should properly create payment with pre-defined config', function () {
     var payment = new BasePayment({
-      apiKey: 'API_KEY'
+      foo: 'bar',
+      obj: {
+        foo: 'bar'
+      }
     });
 
-    assert.equal(payment.getConfig('apiKey'), 'API_KEY');
-    assert.notOk(payment.getConfig('NOT_EXISTS'));
+    assert.equal(payment.get('foo'), 'bar');
+    assert.equal(payment.get('obj.foo'), 'bar');
+    assert.deepEqual(payment.get('obj'), {foo: 'bar'});
+    assert.notOk(payment.get('NOT_EXISTS'));
   });
 
-  it('Should properly get/set payment provider', function () {
+  it('Should properly get/set provider', function () {
     var payment = new BasePayment();
 
     assert.notOk(payment.getProvider());
-    assert.instanceOf(payment.setProvider('SOME_PROVIDER'), BasePayment);
-    assert.equal(payment.getProvider(), 'SOME_PROVIDER');
-  });
-
-  it('Should properly throw error on checkout', function () {
-    var payment = new BasePayment();
-
-    assert.throw(function () {
-      payment.checkout();
-    }, Error);
-  });
-
-  it('Should properly throw error on refund', function () {
-    var payment = new BasePayment();
-
-    assert.throw(function () {
-      payment.refund();
-    }, Error);
+    assert.instanceOf(payment.setProvider('PROVIDER'), BasePayment);
+    assert.equal(payment.getProvider(), 'PROVIDER');
   });
 });
